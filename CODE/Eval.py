02e274cbd8aef3d78605d9a2b029542c83b3aade
Andy Boomer <aboomer07@gmail.com>
2021-03-24 23:52:40 +0100

    Moving

---
+++ CODE/Eval.py
@@ -0,0 +1,5 @@
+#Degress of Freedom = DoF = Tr(M_t^alpha)
+#MSER = MSE Relative to traditional PC
+#RMSFER = RMSE for Forecasting relative to traditional PC (Rolling or Recursive)
+#k average number of factors across simulations
+#alpha = cv tuning parameter
