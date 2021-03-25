4053bf6fa453fede5d32806c6babc279e784428b
jacob_314 <jacob.pichelmann@gmail.com>
2021-03-25 15:07:46 +0100

    First draft of PC, Ridge -- GVC, Mallow!

--- CODE/Methods.py
+++ CODE/Methods.py
@@ -5,6 +5,97 @@ out = gen_sim(sim_params, 'DGP1')
 # Methods to cover:
 # PCA, PLS, Ridge, LF, LASSO,
 
+# run model function notes:
+# Input simulated data, choose model, select penalty parameter choice method and rmax (maximum number of factors)
+# Returns dictionary with
+# 1. Value of penalty selection problem -- key: !!depending on method!!
+# 2. Estimated parameters delta -- key: delta
+# 3. Fitted values y_hat -- key: y_hat
+# 4. Optimal penalty parameter -- key: !!depending on model!!
+
+
+def run_model(data, model, method, rmax):
+    X = data['X']
+    Y = data['y']
+    T = X.shape[0]
+    N = X.shape[1]
+
+    S_xx = (X.T @ X) / T
+    S_xy = (X.T @ Y) / T
+    I = np.identity(N)
+
+    psi_svd, sigma, phi_T_svd = np.linalg.svd(X)
+    psi_svd, sigma, phi_T_svd = psi_svd.real, sigma.real, phi_T_svd.real
+
+    if model == 'PC':
+        if method == 'GCV':
+            GCVs = []
+            for k in range(rmax):
+                psi_a = psi_svd[:, :k]
+                delta_pc = np.linalg.inv(psi_a.T @ psi_a) @ psi_a.T @ Y
+                M_ty = psi_a @ delta_pc
+                GCV = 1 / T * np.linalg.norm(Y - M_ty) ** 2 / (1 - 1 / T * k)
+                GCVs.append({'k': k, 'GCV': GCV, 'delta': delta_pc, 'y_hat': M_ty})
+            opt = min(GCVs, key=lambda x: x['GCV'])
+            print('Model: PC, Method: GCV')
+            print('GCV = ' + str(opt['GCV']) + ' --> k = ' + str(opt['k']))
+            return opt
+
+        if method == 'Mallow':
+            Mallows = []
+            for k in range(rmax):
+                psi_a = psi_svd[:, :k]
+                delta_pc = np.linalg.inv(psi_a.T @ psi_a) @ psi_a.T @ Y
+                M_ty = psi_a @ delta_pc
+                sigma = np.var(Y - M_ty)  # consistent estimator of variance = variance of residuals
+                Mallow = (1/T) * np.linalg.norm(Y - M_ty) ** 2 + 2 * sigma * (1/T) * k
+                Mallows.append({'k': k, 'Mallow': Mallow, 'delta': delta_pc, 'y_hat': M_ty})
+            opt = min(Mallows, key=lambda x: x['Mallow'])
+            print('Model: PC, Method: Mallow')
+            print('Mallow = ' + str(opt['Mallow']) + ' --> k = ' + str(opt['k']))
+            return opt
+
+    if model == 'Ridge':
+        if method == 'GCV':
+            GCVs = []
+            for alpha in np.linspace(0, 1, 100): # change this!!!
+                delta_ridge = np.linalg.inv(S_xx + alpha * I) @ S_xy
+                M_ty = X @ delta_ridge
+                M_t = X @ np.linalg.inv(S_xx + alpha * I) @ (X.T/T)
+                GCV = 1 / T * np.linalg.norm(Y - M_ty) ** 2 / (1 - 1 / T * np.trace(M_t))
+                GCVs.append({'GCV': GCV, 'delta': delta_ridge, 'y_hat': M_ty, 'alpha': alpha})
+            opt = min(GCVs, key=lambda x: x['GCV'])
+            print('Model: Ridge, Method: GCV')
+            print('Optimal alpha = ' + str(opt['alpha']) + ' with GCV = ' + str(opt['GCV']))
+            return opt
+
+        if method == 'Mallow':
+            Mallows = []
+            for alpha in np.linspace(0, 1, 100): # change this!!!
+                delta_ridge = np.linalg.inv(S_xx + alpha * I) @ S_xy
+                M_ty = X @ delta_ridge
+                M_t = X @ np.linalg.inv(S_xx + alpha * I) @ (X.T/T)
+                sigma = np.var(Y - M_ty)  # consistent estimator of variance = variance of residuals
+                Mallow = (1/T) * np.linalg.norm(Y - M_ty) ** 2 + 2 * sigma * (1/T) * np.trace(M_t)
+                Mallows.append({'Mallow': Mallow, 'delta': delta_ridge, 'y_hat': M_ty, 'alpha': alpha})
+            opt = min(Mallows, key=lambda x: x['Mallow'])
+            print('Model: Ridge, Method: Mallow')
+            print('Optimal alpha = ' + str(opt['alpha']) + ' with Mallow = ' + str(opt['Mallow']))
+            return opt
+
+
+
+
+mods = run_model(out, 'PC', 'GCV', rmax=10)
+mods = run_model(out, 'PC', 'Mallow', rmax=10)
+mods = run_model(out, 'Ridge', 'GCV', rmax=10)
+mods = run_model(out, 'Ridge', 'Mallow', rmax=10)
+
+
+
+################### LEGACY/EXPERIMENT CODE #####################
+
+# legacy method with straight forward eigenvectors for PCA (no SVD)
 S_xx = np.matmul(out['X'], out['X'].T)/200
 lambda_sq, psi = np.linalg.eig(S_xx)
 lambda_sq = lambda_sq.real
@@ -19,13 +110,11 @@ delta_hat = np.linalg.inv(new_psi.T @ new_psi) @ new_psi.T @ out['y']
 
 [np.absolute(i) for i in lambda_sq][:10]  # check magnitude of complex part
 
-# for now: phi = psi
 
-# M_t * y = y_hat
-
-# what is lambda without sq?
+# cross checking the equivalence between eigenvectors and SVD
 
 X = out['X']
+T = X.shape[0]
 S_xx = (X.T @ X) / T
 S_xx_2 = (X @ X.T) / T
 
@@ -48,27 +137,3 @@ alpha = 100
 psi_a = psi_svd[:, list(np.where(sigma >= alpha))]
 psi_a = psi_a.reshape((psi_a.shape[0], psi_a.shape[2]))
 delta_pc_a = np.linalg.inv(psi_a.T @ psi_a) @ psi_a.T @ out['y']
-
-
-def get_alpha(data, model, rmax):
-    X = data['X']
-    Y = data['y']
-    T = X.shape[0]
-
-    psi_svd, sigma, phi_T_svd = np.linalg.svd(X)
-    psi_svd, sigma, phi_T_svd = psi_svd.real, sigma.real, phi_T_svd.real
-
-    if model == 'PC':
-        GCVs = []
-        for k in range(rmax):
-            psi_a = psi_svd[:, :k]
-            delta_pc = np.linalg.inv(psi_a.T @ psi_a) @ psi_a.T @ Y
-            M_ty = psi_a @ delta_pc
-            GCV = 1 / T * np.linalg.norm(Y - M_ty) ** 2 / (1 - 1 / T * k)
-            GCVs.append((k, GCV))
-        return GCVs
-
-get_alpha(out, 'PC', rmax = 10)
-
-#  optimal k is 4!!!
-
