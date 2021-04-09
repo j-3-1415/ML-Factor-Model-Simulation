02d5edf0dbe9836d4250bf1419bfc5e56cfa8a35
Andy Boomer <aboomer07@gmail.com>
2021-04-09 12:25:34 -0700

    Merging

--- CODE/Methods.py
+++ CODE/Methods.py
@@ -1,6 +1,7 @@
 # from CODE.DataSim import *
 from DataSim import *
 import operator
+import pandas as pd
 
 # Methods to cover:
 # PCA, PLS, Ridge, LF, LASSO,
@@ -25,6 +26,7 @@ def get_model_output(data, model, iteration):
     r_max = data['r_max']
 
     S_xx = (X.T @ X) / T
+    S_xxT = (X @ X.T) / T
     S_xy = (X.T @ y) / T
     I = np.identity(S_xx.shape[0])
 
@@ -32,22 +34,18 @@ def get_model_output(data, model, iteration):
     psi_svd, sigma, phi_T_svd = psi_svd.real, sigma.real, phi_T_svd.real
 
     lambda_sq, psi = np.linalg.eig(S_xx)
+    lambda2, psi2 = np.linalg.eig(S_xxT)
     lambda_sq, psi = lambda_sq.real, psi.real
 
     if model == 'PC':
     	psi_svd = psi_svd[:, :data['k'][iteration]]
     	M_t = psi_svd @ np.linalg.inv(psi_svd.T @ psi_svd) @ psi_svd.T
-    	# M_ty = M_t @ y
 
     elif model == 'Ridge':
     	q = lambda_sq / (lambda_sq + data['alphas'][iteration])
     	q = q.reshape(1, q.shape[0])
     	M_t = psi_svd @ np.linalg.inv(psi_svd.T @ psi_svd) @ psi_svd.T
     	M_t = np.multiply(q, M_t[:, :, np.newaxis]).sum(axis=2)
-
-    	# alphaI = data['alphas'][iteration] * np.identity(N)
-    	# M_t = X @ np.linalg.inv(S_xx + alphaI) @ (X.T * (1/T))
-    	# M_ty = M_t @ y
 
     elif model == 'LF':
     	alpha = data['alphas'][iteration]
@@ -56,14 +54,25 @@ def get_model_output(data, model, iteration):
         q = (q / lambda_sq).reshape(1, q.shape[0])
         M_t = psi_svd @ psi_svd.T
         M_t = X @ X.T @ np.multiply(q, M_t[:, :, np.newaxis]).sum(axis=2)
-        # M_ty = M_t @ y
 
     elif model == 'PLS':
     	V_k = np.tile(X.T @ y, (1, data['k'][iteration]))
     	for i in range(1, data['k'][iteration]):
     		V_k[:, i] = np.power(X.T @ X, i) @ V_k[:, i]
     	M_t = X @ V_k @ np.linalg.inv(V_k.T @ X.T @ X @ V_k) @ V_k.T @ X.T
-		# M_ty = M_t @ y
+
+    elif model == 'BaiNg':
+
+    	F = np.sqrt(T) * psi2[:, :data['k'][iteration]]
+    	lamb = (1 / T) * (F.T @ X)
+    	V = 0
+    	for i in range(N):
+    		for t in range(T):
+    			V += np.power(X[t, i] - (lamb[:, i] @ F[t, :]), 2)
+    	V = (1 / (N * T)) * V
+    	PC = V + (data['k'][iteration] * (2 / T))
+    	data['criteria'][iteration] = PC
+    	return(data)
 
     data['M_t'] = M_t
     data['M_ty'] = M_t @ y
@@ -74,6 +83,9 @@ def cv(data, model, method, iteration):
     data = data.copy()
 
     data = get_model_output(data, model, iteration)
+
+    if model == 'BaiNg':
+    	return(data)
 
     M_t = data['M_t']
 	M_ty = data['M_ty']
@@ -111,11 +123,7 @@ def cv(data, model, method, iteration):
     	diag = 1 - np.diag(M_t)
     	crit = (1 / T) * np.sum([(errors[i] / diag[i])**2 for i in range(len(errors))])
 
-    	# crit = (1 / T) * np.sum(np.power(errors / (1 - np.diag(M_t)), 2))
-
     data['criteria'][iteration] = crit
-
-
 
     return(data)
 
@@ -128,12 +136,13 @@ def monte_carlo(monte_params, sim_params):
 	best = []
 	model = monte_params['model']
 	eval_form = monte_params['eval']
-	crit_var = ['alphas', 'k'][model in ['PC', 'PLS']]
+	crit_var = ['alphas', 'k'][model in ['PC', 'PLS', 'BaiNg']]
 	r_max = sim_params['r_max'][DGP-1]
 
 	crit_dict = {'k' : 
 		{'PC' : [list(range(0, (r_max + 1)))] * 6,
-		'PLS' : [list(range(1, (r_max + 1)))] * 6},
+		'PLS' : [list(range(1, (r_max + 1)))] * 6,
+		'BaiNg' : [list(range(0, (r_max + 1)))] * 6},
 		'alphas' :
 		{'Ridge' : [N * np.arange(0, 0.1, 0.001), 
 					N * np.arange(0, 0.01, 0.0001),
@@ -176,13 +185,24 @@ def monte_carlo(monte_params, sim_params):
 monte_params = {
 	'N' : 100,
 	'T' : 50,
-	'sims' : 1000,
+	'sims' : 25,
 	'DGP' : 1,
-	'model' : 'PLS',
+	'model' : 'BaiNg',
 	'eval' : 'LOO_CV'
 }
 
 monte_carlo(monte_params, sim_params)
 
+
+sizes = ['500x200', '100x50']
+models = ['PC', 'PLS', 'Ridge', 'LF', 'BaiNg']
+evals = ['GCV', 'Mallow', 'AIC', 'BIC', 'LOO_CV']
+comb = [[i, j, k] for i in sizes for j in models for k in evals]
+check = pd.DataFrame(comb)
+check.columns = ['TxN', 'Model', 'Eval']
+check.loc[:, ['DGP' + str(i+1) for i in range(6)]] = 'No'
+check = check[~((check['Model']!='PLS')&(check['Eval'] == 'LOO_CV'))].reset_index().drop('index', axis=1)
+check.loc[check['Model'] == 'BaiNg', 'Eval'] = 'BaiNg'
+check = check[~check.duplicated()].reset_index().drop('index', axis=1)
 
 
