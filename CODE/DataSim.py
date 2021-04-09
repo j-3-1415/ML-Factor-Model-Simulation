7c78532c8e77559069afa20246595705165c1368
Andy Boomer <aboomer07@gmail.com>
2021-04-08 22:51:45 -0700

    Updated sim params

--- CODE/DataSim.py
+++ CODE/DataSim.py
@@ -18,13 +18,13 @@ def gen_params(sim_params, N, T):
 
 	M = np.divide(np.ones((N, N)), np.array(range(1, (N + 1))).reshape((N, 1)))
 
-	mean = np.zeros(N)
-	covar = np.multiply(np.array([1, 2, 3, 3, 4] + [1] * (N - 5)).reshape((1, N)), np.identity(N))
+	mean = np.zeros(r[2])
+	covar = np.multiply(np.array([1, 2, 3, 3, 4]), np.identity(r[2]))
 
 	params = {'DGP1' : {'theta' : np.ones((r[0], 1)), 'nu' : np.random.normal(0, 1, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.normal(0, 1, (T, r[0])), 'lambda' : np.random.normal(0, 1, (r[0], N))},
 	'DGP2' : {'theta' : np.ones((r[1], 1)), 'nu' : np.random.normal(0, 1, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.normal(0, 1, (T, r[1])), 'lambda' : np.random.normal(0, 1, (r[1], N))},
-	'DGP3' : {'theta' : np.array([1] + [0] * (r[2] - 1)).reshape((r[2], 1)), 'nu' : np.random.normal(0, 0.01, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.multivariate_normal(mean, covar, 10), 'lambda' : np.random.normal(0, 1, (r[2], N))},
-	'DGP4' : {'theta' : np.zeros((r[3], 1)), 'nu' : np.random.normal(0, 1, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.multivariate_normal(mean, covar, 10), 'lambda' : np.random.normal(0, 1, (r[3], N))},
+	'DGP3' : {'theta' : np.array([1] + [0] * (r[2] - 1)).reshape((r[2], 1)), 'nu' : np.random.normal(0, 0.01, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.multivariate_normal(mean, covar, T), 'lambda' : np.random.normal(0, 1, (r[2], N))},
+	'DGP4' : {'theta' : np.zeros((r[3], 1)), 'nu' : np.random.normal(0, 1, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.multivariate_normal(mean, covar, T), 'lambda' : np.random.normal(0, 1, (r[3], N))},
 	'DGP5' : {'theta' : np.zeros((r[4], 1)), 'nu' : np.random.normal(0, 1, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.normal(0, 1, (T, r[4])), 'lambda' : np.multiply(M, np.random.normal(0, 1, (N, N)))},
 	'DGP6' : {'theta' : np.ones((r[5], 1)), 'nu' : np.random.normal(0, 1, (T, 1)), 'xi' : np.random.normal(0, 1, (T, N)), 'F' : np.random.normal(0, 1, (T, r[5])), 'lambda' : (1 / np.sqrt(N)) * np.ones((r[5], N))}
 
@@ -42,14 +42,14 @@ def gen_sim(sim_params, dgp, N, T):
 	nu = params[dgp]['nu']
 	xi = params[dgp]['xi']
 
-	sim_output = {'y' : np.matmul(F, theta) + nu, 'X' : np.matmul(F, Lambda) + xi}
+	sim_output = {'y' : F @ theta + nu, 'X' : F @ Lambda + xi}
 
 	sim_output['r_max'] = sim_params['r_max'][int(dgp[-1]) - 1]
 
 	return(sim_output)
 
 
-out = gen_sim(sim_params, 'DGP1', 200, 500)
+out = gen_sim(sim_params, 'DGP3', 200, 500)
 
 
 
