db6b3e18d71da0f2166757a2d40fcfd8805cd1e3
Andy Boomer <aboomer07@gmail.com>
2021-04-16 22:41:39 -0700

    Updated function to take in data dictionary

--- CODE/OutputTex.py
+++ CODE/OutputTex.py
@@ -1,30 +1,57 @@
 # 3. Function to output latex regression table
-def out_latex(file):
+def out_latex(file, tex_dict):
 
     ############################################################################
     # Input Notes
     ############################################################################
 
     string = """
-\\begin{center} $(\\mathrm{GCV}, N=200, T=500)$ \\\\
-\\begin{tabular}{cccccccc}
-\\hline \\hline & & PC & PLS & \\multicolumn{Ridge}{2} & \\multicolumn{LF}{2}\\\\
-\\hline & $r$ & $k$ & $k$ & $\\alpha$ & \\\\
-\\hline DGP 1 & $4.00$ & $4.00$ & $4.83$ \\\\
-(s.e.) & $-$ & $(0.00)$ & $(1.90)$ \\\\
-DGP 2 & $50.00$ & $200.00$ & $50.96$ \\\\
-(s.e.) & $-$ & $(0.00)$ & $(2.59)$\\\\
-DGP 3 & $5.00$ & $5.61$ & $1.93$ \\\\
-(s.e.) & $-$ & $(0.71)$ & $(2.11)$ \\\\
-DGP 4 & $5.00$ & $5.61$ & $0.86$\\\\
-(s.e.) & $-$ & $(0.75)$ & $(1.90)$ \\\\
-DGP 5 & $200.00$ & $200.00$ & $11.94$\\\\
-(s.e.) & $-$ & $(0.00)$ & $(8.27)$\\\\
-DGP 6 & $1.00$ & $0.00$ & $6.51$\\\\
-(s.e.) & $-$ & $(0.00)$ & $(3.38)$\\\\
-\\hline
-\\end{tabular}
-\\end{center}
+    \\begin{center} $(\\mathrm{%s}, N=%s, T=%s)$ \\
+        \\begin{tabular}{cccccccc}
+            \\hline \\hline 
+            & & PC & PLS & \\multicolumn{2}{c}{Ridge} & \\multicolumn{2}{c}{LF} \\\\
+            \\hline 
+            & $r$ & $k$ & $k$ & $\\alpha$ & DOF & $\\alpha$ & DOF \\\\
+            \\hline 
+            DGP 1 & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ \\\\
+            (s.e.) & $-$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ \\\\
+            DGP 2 & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ \\\\
+            (s.e.) & $-$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ \\\\
+            DGP 3 & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ \\\\
+            (s.e.) & $-$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ \\\\
+            DGP 4 & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ \\\\
+            (s.e.) & $-$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ \\\\
+            DGP 5 & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ \\\\
+            (s.e.) & $-$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ \\\\
+            DGP 6 & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ & $%s$ \\\\
+            (s.e.) & $-$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ & $(%s)$ \\\\
+            \\hline
+        \\end{tabular}
+    \\end{center}
     """
 
-    return(string)
+    tex = tex_dict.copy()
+
+    data = [tex['method'], tex['N'], tex['T']]
+
+    for i in range(6):
+        data += ['{0:.2f}'.format(tex['r'][i])]
+        data += ['{0:.2f}'.format(tex['PC']['params'][i])]
+        data += ['{0:.2f}'.format(tex['PLS']['params'][i])]
+        data += ['{0:.2f}'.format(tex['Ridge']['alpha']['params'][i])]
+        data += ['{0:.2f}'.format(tex['Ridge']['DOF']['params'][i])]
+        data += ['{0:.2f}'.format(tex['LF']['alpha']['params'][i])]
+        data += ['{0:.2f}'.format(tex['LF']['DOF']['params'][i])]
+        data += ['{0:.2f}'.format(tex['PC']['se'][i])]
+        data += ['{0:.2f}'.format(tex['PLS']['se'][i])]
+        data += ['{0:.2f}'.format(tex['Ridge']['alpha']['se'][i])]
+        data += ['{0:.2f}'.format(tex['Ridge']['DOF']['se'][i])]
+        data += ['{0:.2f}'.format(tex['LF']['alpha']['se'][i])]
+        data += ['{0:.2f}'.format(tex['LF']['DOF']['se'][i])]
+
+    data = tuple(data)
+
+    string = string%data
+
+    with open(file, 'w') as f:
+        f.write(string)
